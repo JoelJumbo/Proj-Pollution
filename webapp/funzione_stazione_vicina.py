73a1c72b2ffaa8bbc39c282fd837b3d29b489843
JoelJumbo <77815777+JoelJumbo@users.noreply.github.com>
2024-03-20 13:11:57 +0100

    Zero
    First part of the project that needs to be finished

---
+++ webapp/funzione_stazione_vicina.py
@@ -0,0 +1,89 @@
+# # importing geopy library
+# from geopy.geocoders import Nominatim
+# # Importing the geodesic module from the library
+# from geopy.distance import geodesic
+#
+#
+# #Valore preso dalla pagina html
+# location = " "
+#
+# location_stazioni = " "
+#
+# # SELECT id_stazione, latitudine, longitudine FROM `stazione`;
+# # calling the Nominatim toolion
+# loc = Nominatim(user_agent="GetLoc")
+#
+# #Individuo la stazione piú vicina alla location inserita
+#
+# # entering the location name
+# getLoc1 = loc.geocode(location)
+# print(getLoc1.address)
+# getLoc2 = loc.geocode(location2)
+# print(getLoc2.address)
+#
+# # # printing address
+# # print(getLoc.address)
+#
+# # Print the distance calculated in km
+# print(geodesic((getLoc1.latitude,getLoc1.longitude), (getLoc2.latitude, getLoc2.longitude) ).km)
+# #
+# # # printing latitude and longitude
+# # print("Latitude = ", getLoc.latitude, "\n")
+# # print("Longitude = ", getLoc.longitude)
+
+
+# importing geopy library
+import pandas as pd
+import numpy
+#from geopy.geocoders import Nominatim
+# Importing the geodesic module from the library
+#from geopy.distance import geodesic
+
+def calcolo_distanza(tupla):
+    var = pd.read_csv("../migration/database_sql/data/data_clean/dataset_pulito_stazioni.csv", encoding='latin-1')
+    minima = 1000
+    id_stazione = 0
+    comune = ""
+    for i,elem in var.iterrows():
+        latitudine = elem["lat"]
+        longitudine = elem["lng"]
+        dist = numpy.sqrt((latitudine - float(tupla[0]))**2 + (longitudine - float(tupla[1]))**2)
+        if dist < minima:
+            minima = dist
+            id_stazione = elem["IdStazione"]
+            comune = elem["Comune"]
+    #print(minima, id_stazione, comune)
+    return id_stazione
+#
+# df = var.loc[var['denominazione'] == comune]
+# # print(ciao)
+# latitudine = df['latitudine'].values[0]
+# longitudine = df['longitudine'].values[0]
+# tupla = (str(latitudine), str(longitudine))
+# print(tupla)
+# #Valore preso dalla pagina html
+# location = " "
+#
+# location_stazioni = " "
+#
+# # SELECT id_stazione, latitudine, longitudine FROM `stazione`;
+# # calling the Nominatim tool
+# loc = Nominatim(user_agent="GetLoc")
+#
+# #Individuo la stazione piÃº vicina alla location inserita
+#
+# # entering the location name
+# getLoc1 = loc.geocode(location)
+# print(getLoc1.address)
+# getLoc2 = loc.geocode(location2)
+# print(getLoc2.address)
+#
+# # # printing address
+# # print(getLoc.address)
+#
+# # Print the distance calculated in km
+# print(geodesic((getLoc1.latitude,getLoc1.longitude), (getLoc2.latitude, getLoc2.longitude) ).km)
+# #
+# # # printing latitude and longitude
+# # print("Latitude = ", getLoc.latitude, "\n")
+# # print("Longitude = ", getLoc.longitude)
---
+++ webapp/try.bat
@@ -0,0 +1,6 @@
+call .venv\Scripts\activate.bat
+SET FLASK_APP=test.py
+SET MAIL_USERNAME=pollushow@gmail.com
+SET MAIL_PASSWORD=zvyy xvft sroq kgtt
+flask run --host=0.0.0.0 --port=58320
+pause
